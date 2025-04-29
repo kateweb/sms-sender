98eddcdd892d3038174c5d007729a17c114da964
kateburlachenko <k.burlachenko@treeum.net>
2025-04-29 14:06:23 +0300

    SDD-6690 fix packages

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,11 +1,13 @@
 import { Center, Loader, Stack, Text } from '@mantine/core';
+import { useTranslations } from 'next-intl';
 
 export default function Loading() {
+  const t = useTranslations();
   return (
     <Center pt={80}>
       <Stack align="center">
         <Loader />
-        <Text>Hang in there...</Text>
+        <Text>{t('loading')}...</Text>
       </Stack>
     </Center>
   );
